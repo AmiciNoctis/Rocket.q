5d11ec98f85533a3dbe93e2bc03d1d9e2cdf8cf4
Felipe Barbosa <felipeslav23@gmail.com>
2021-07-09 14:25:07 -0300

    09/07

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,24 +5,24 @@ const modal = Modal();
 const modalTitle = document.querySelector('.modal h2');
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button');
-
 const deleteButtons = document.querySelectorAll('.actions a.excluir');
-
-deleteButtons.forEach(button => {
-  button.addEventListener("click", event => {
-    modalTitle.innerHTML = "Excluir pergunta";
-
-    modal.open();
-  });
-});
-
 const checkButtons = document.querySelectorAll('.actions a.check');
 
 checkButtons.forEach(button => {
-  button.addEventListener("click", event => {
-    modalTitle.innerHTML = "Marcar como lida";
-    modalDescription.innerHTML = ""
-
-    modal.open();
-  });
+  button.addEventListener("click", handleClick)
 });
+
+deleteButtons.forEach(button => {
+  button.addEventListener("click", (event) => handleClick(event, false));
+})
+
+function handleClick(event, check = true) {
+  const text = check ? "Marcar como lida" : "Excluir";
+
+  modalTitle.innerHTML = `${text} esta pergunta`;
+  modalDescription.innerHTML = `Tem certeza que você deseja ${text.toLowerCase()} esta pergunta ?`;
+  modalButton.innerHTML = check ? "Mensagem lida":"Sim, excluir";
+  modalButton.className = check ? "button":"button red";
+
+  modal.open();
+}
