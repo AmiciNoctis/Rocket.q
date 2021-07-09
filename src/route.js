474e0f314fb751432a285d8ad63cd52f5c22b549
Felipe Barbosa <felipeslav23@gmail.com>
2021-07-09 18:30:33 -0300

    db

--- src/route.js
+++ src/route.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const questionController = require('./controllers/questionController');
+const roomController = require('./controllers/roomController');
 
 const route = express.Router();
 
-route.get('/', (req, res) => res.render("index"));
-route.get('/create-room', (req, res) => res.render("create-room"));
-route.get('/room', (req, res) => res.render("room"));
-route.get('/room/:room/:question/:action', (req, res) => res.render("exemplo", {req}));
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
+route.get('/create-room', (req, res) => res.render("index", {page: 'create-room'}));
+
+route.get('/room/:roomId', (req, res) => res.render("room"));
+
+route.post('/create-room/', roomController.create);
+route.post('/question/:room/:question/:action', questionController.index);
 
 module.exports = route;
