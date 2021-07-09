287cb113f8c9488f247ccb9f1318711e7e7011e1
Felipe Barbosa <felipeslav23@gmail.com>
2021-07-09 15:52:15 -0300

    question route

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -17,11 +17,18 @@ deleteButtons.forEach(button => {
 })
 
 function handleClick(event, check = true) {
+  event.preventDefault();
   const text = check ? "Marcar como lida" : "Excluir";
+  const slug = check ? "check" : "delete"; 
+  const roomId = document.querySelector("#room-id").dataset.id;
+  const questionId = event.target.dataset.id;
+
+  const form = document.querySelector(".modal form");
+  form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`);
 
   modalTitle.innerHTML = `${text} esta pergunta`;
   modalDescription.innerHTML = `Tem certeza que você deseja ${text.toLowerCase()} esta pergunta ?`;
-  modalButton.innerHTML = check ? "Mensagem lida":"Sim, excluir";
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
   modalButton.className = check ? "button":"button red";
 
   modal.open();
--- src/route.js
+++ src/route.js
@@ -5,5 +5,6 @@ const route = express.Router();
 route.get('/', (req, res) => res.render("index"));
 route.get('/create-room', (req, res) => res.render("create-room"));
 route.get('/room', (req, res) => res.render("room"));
+route.get('/room/:room/:question/:action', (req, res) => res.render("exemplo", {req}));
 
 module.exports = route;
