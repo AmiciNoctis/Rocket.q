cd83d57895180d468451fd993cd68c16744364f4
Felipe Barbosa <felipeslav23@gmail.com>
2021-11-14 20:36:32 -0300

    room fix

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -2,28 +2,42 @@ const Database = require('../db/config');
 
 module.exports = {
   async create(req, res){
-    const db = await Database();
+      const db = await Database()
+      const pass = req.body.password
+      let roomId
+      let isRoom = true
+      while(isRoom){
+          /* Gera o numero da sala */
+          for(var i = 0; i < 6; i++){
+              i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+              roomId += Math.floor(Math.random() * 10).toString()
+          }
 
-    const pass = req.body.password;
+          /* Verificar se esse numero ja existi */
+          const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+          isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
 
-    let roomId;
+          if(!isRoom){
+              /* Inseri a sala no banco */
+              await db.run(`INSERT INTO rooms (
+                  id,
+                  pass
+              ) VAlUES (
+                  ${parseInt(roomId)},
+                  ${pass}
+              )`)
+          }
+      }         
 
-    for(var i = 0; i<6; i++) {
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-      roomId += Math.floor(Math.random() * 10).toString();
-    }
+      await db.close()
 
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
+      res.redirect(`/room/${roomId}`)
+  },
 
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`);  
-
-    await db.close();
-
-    res.redirect(`/room/${roomId}`);
+  async open(req, res) {
+    const db = await Database()
+    const roomId = req.params.roomId
+    res.render("room", {roomId: roomId})
+    console.log(`${roomId}`)
   }
 }
--- src/route.js
+++ src/route.js
@@ -7,7 +7,7 @@ const route = express.Router();
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
 route.get('/create-room', (req, res) => res.render("index", {page: 'create-room'}));
 
-route.get('/room/:roomId', (req, res) => res.render("room"));
+route.get('/room/:roomId', roomController.open);
 
 route.post('/create-room/', roomController.create);
 route.post('/question/:room/:question/:action', questionController.index);
